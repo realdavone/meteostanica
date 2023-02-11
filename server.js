89dc8badba26d713cf30fc47249cee0bc7ed86fc
realdavone <davidkuljovsky@gmail.com>
2023-02-11 12:12:14 +0100

    dist folder now being served at / path

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import path from 'path';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 import tempRoutes from './routes/temp.js'
 import authRoutes from './routes/auth.js'
@@ -9,14 +14,22 @@ import authRoutes from './routes/auth.js'
 dotenv.config()
 
 const app = express()
+const port = process.env.PORT || 5000
 
 mongoose.connect(process.env.DATABASE).then(() => { console.log('DB CONNECTED') })
 
 app.use(express.json())
 app.use(cors())
+
 app.use('/temp', tempRoutes)
 app.use('/auth', authRoutes)
 
-app.listen(5000, () => {
-  console.log('Server running')
+app.use(express.static(__dirname + '/dist'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`)
 })
